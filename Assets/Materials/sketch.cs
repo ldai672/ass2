e5e7bf3d966b6c02ddff03e021729023c38f8ae2
ldai672 <ldai672@aucklanduni.ac.nz>
2016-05-25 10:56:23 +1200

    Update sketch.cs

--- Assets/Materials/sketch.cs
+++ Assets/Materials/sketch.cs
@@ -1,1 +1,34 @@
+using UnityEngine;
+using System.Collections;
 
+public class sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        int totalCubes = 12;
+
+        int totalDistance = 5;
+
+        for ( int i =0; i<6; i++)
+        {
+            float perc = i / (float)totalCubes;
+            float x = perc*totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<cubescript>().SetSize(1.0f-perc);
+            newCube.GetComponent<cubescript>().rotateSpeed =perc; //Random.value;
+        }
+       
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
