2ba7b48c41ed31d32134933fc6d8b7d52672c17d
ldai672 <ldai672@aucklanduni.ac.nz>
2016-05-30 13:48:54 +1200

    Update sketch.cs

--- sketch.cs
+++ sketch.cs
@@ -15,13 +15,15 @@ public class sketch : MonoBehaviour {
         for ( int i =0; i<6; i++)
         {
             float perc = i / (float)totalCubes;
-            float x = perc*totalDistance;
+            float sin = Mathf.Sin(perc *Mathf.PI/2);
+            
+            float x = 1.0f +Sin *totalDistance;
             float y = 5.0f;
             float z = 0.0f;
 
             var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-            newCube.GetComponent<cubescript>().SetSize(1.0f-perc);
-            newCube.GetComponent<cubescript>().rotateSpeed =perc; //Random.value;
+            newCube.GetComponent<cubescript>().SetSize(1f * (1.0f-perc));
+            newCube.GetComponent<cubescript>().rotateSpeed =0.2f + perc*4.0f; 
         }
        
        
